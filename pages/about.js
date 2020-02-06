bd9c839487793cb5a6eb6b84135ec8027feb7776
stefan@arteries <stefan@arteries.hu>
2020-02-06 10:04:22 +0100

    setup | base components and shared

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+import Header from "../components/Header";
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+
+      <h1>About</h1>
+    </div>
+  );
+}
