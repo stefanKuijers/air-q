28503436465f22ac636e4be00d86df5121706f66
stefan@arteries <stefan@arteries.hu>
2020-02-14 23:08:42 +0100

    openAQ | basic implementation

---
+++ data/openaq/api.ts
@@ -0,0 +1,17 @@
+import fetch from 'isomorphic-unfetch';
+import qs from 'qs';
+
+type Resource = 'measurements' | 'cities';
+type Options = { [key: string]: string | number | string[] };
+
+async function get(resource: Resource, options?: Options): Promise<any[]> {
+    const optionsString = options ? `?${qs.stringify(options, { encode: false })}` : '';
+    const response = await fetch(`https://api.openaq.org/v1/${resource}${optionsString}`);
+    const data = await response.json();
+
+    return data.results;
+}
+
+export default {
+    get,
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,60 @@
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Typography, Grid } from '@material-ui/core';
 
+import OpenAQ from '../data/openaq/api';
 import MeasurementCard from '../components/measurement-card/MeasurementCard';
 import Layout from '../components/Layout';
 
-const Home: NextPage = () => {
-    const city = 'Budapest';
+const Page: NextPage = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const [airQuality, setAirQuality] = useState([] as any[]);
+
+    useEffect((): void => {
+        OpenAQ.get('measurements', {
+            coordinates: '47.49,19.04',
+            // radius: 1000,
+            // it seems there is no pm25 close to budapest
+            parameter: ['pm25', 'pm10', 'so2'],
+            limit: 5,
+        }).then(data => {
+            setAirQuality(data);
+        });
+    }, []);
+
     return (
         <Layout>
-            <Box component="header" mt={5} mb={5}>
-                <Typography variant="h2">{city}</Typography>
-            </Box>
-            <Grid container spacing={4}>
-                <Grid item xs={6}>
-                    <MeasurementCard label="PM2.5" value={12.4} />
-                </Grid>
-                <Grid item xs={6}>
-                    <MeasurementCard label="PM10" value={9.7} />
-                </Grid>
-            </Grid>
+            {airQuality.length ? (
+                <>
+                    <Box component="header" mt={5} mb={5}>
+                        <Typography variant="h2">{airQuality.length}</Typography>
+                    </Box>
+                    <Grid container spacing={4}>
+                        <Grid item xs={6}>
+                            <MeasurementCard label="PM2.5" value={12.4} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <MeasurementCard label="PM10" value={9.7} />
+                        </Grid>
+                    </Grid>
+                </>
+            ) : (
+                <span>loading</span>
+            )}
         </Layout>
     );
 };
 
-export default Home;
+// Page.getInitialProps = async function(): Promise<any> {
+//     const data = await OpenAQ.get('measurements', {
+//         coordinates: '40.23,34.17',
+//     });
+
+//     console.log(`Show data fetched. Count: ${data.length}`, data);
+
+//     return {
+//         data,
+//     };
+// };
+
+export default Page;
